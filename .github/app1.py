4035c693888a4e61a856f35ef05ccc846fd590a4
thetelgote <sushanttelgote4@gmail.com>
2025-04-02 23:47:21 +0530

    the code will be added

---
+++ .github/app1.py
@@ -0,0 +1,15 @@
+def hello():
+    return "Hello, CI/CD!"
+
+if __name__ == "__main__":
+    print(hello())
+
+import unittest
+from app import hello
+
+class TestApp(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(hello(), "Hello, CI/CD!")
+
+if __name__ == "__main__":
+    unittest.main()
